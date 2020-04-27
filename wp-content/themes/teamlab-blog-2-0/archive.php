dcc3426f920b2ff7496ebdd698c52bd5e00c74dd
MikhailTyuftyaev <tyuftiaev@mail.ru>
2020-04-27 12:04:52 +0300

    Add page Tags and edit styles in widget recent post

--- wp-content/themes/teamlab-blog-2-0/archive.php
+++ wp-content/themes/teamlab-blog-2-0/archive.php
@@ -10,44 +10,52 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+		<main>
+			<div class="SingleContainer">
+		
+		<div id="content" role="main">
+			<div class="content">
+			<?php if ( have_posts() ) : ?>
+			<?php
+			add_filter( 'get_the_archive_title', function( $title ){
+			return preg_replace('~^[^:]+: ~', '', $title );
+			});?>
+			<h2 class="entry-title">Tag Archives:
+			<?php
+			the_archive_title( '<a class="nameTags">#', '</a>' );
+			?></h2>
+			
+				
 
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
 				/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content-tag', get_post_type() );
 
 			endwhile;
 
-			the_posts_navigation();
-
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+			get_template_part( 'template-parts/content-tag', 'none' );
 
 		endif;
 		?>
 
+
+
+					</div><!-- #content -->
+					<div class="sidebar-press">
+				<?php dynamic_sidebar('sidebar-2'); ?>
+				</div>
+				</div><!-- .content -->
+			</div><!-- SingleContainer -->
 		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
---
+++ wp-content/themes/teamlab-blog-2-0/template-parts/content-tag.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template part for tags posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Teamlab_Blog_2.0
+ */
+?>
+
+				<article class="post tags-page">
+
+				<div class="meta">
+						<span class="date"><?php echo get_the_date('j F Y'); ?></span>
+						<span class="autor"><?php _e('By') ?> <?php echo get_the_author(); ?></span>
+				</div>
+				 <h2 class="entry-title tags-page-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'tmblog' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+				
+				 <p><?php the_excerpt() ?></p>
+				 </article><?php
