b955fa4047f096ee0de2dc126c27621082abb3b8
MikhailTyuftyaev <tyuftiaev@mail.ru>
2020-04-29 10:56:27 +0300

    Edit permalink v.2.0. in page "in the press"

--- wp-content/themes/teamlab-blog-2-0/in-the-press.php
+++ wp-content/themes/teamlab-blog-2-0/in-the-press.php
@@ -43,12 +43,12 @@ get_header();
 				
 				<div class="meta press-page">
 						<?php if(get_field('URL')){
-					            echo '<span><a class="press-url" href="http://'.get_field('URL').'/" target="_blank">'.get_field('URL').'</a></span>';
+					            echo '<span><a class="press-url" href="'.get_field('URL').'" target="_blank">'.get_field('URL').'</a></span>';
 					        }
 					    ?>
 						<span class="date"><?php echo get_the_date('j F Y'); ?></span>
 				</div>
-				<h2 class="entry-title press-page-title"><a href="http://<?php echo get_field('URL')?>/" target="_blank" title="<?php printf( esc_attr__( 'Permalink to %s', 'tmblog' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+				<h2 class="entry-title press-page-title"><a href="<?php echo get_field('URL')?>" target="_blank" title="<?php printf( esc_attr__( 'Permalink to %s', 'tmblog' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 				 <p><?php the_excerpt() ?></p>
 				 </article>
