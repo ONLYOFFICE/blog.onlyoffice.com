e031edd8516b2a639500ccea4be710d38dc09f54
MikhailTyuftyaev <tyuftiaev@mail.ru>
2020-08-14 18:00:33 +0300

    Fixes from the designer

--- wp-content/themes/teamlab-blog-2-0/loadmore-tag.php
+++ wp-content/themes/teamlab-blog-2-0/loadmore-tag.php
@@ -2,7 +2,7 @@
 
 				<div class="meta">
 						<span class="date"><?php echo get_the_date('j F Y'); ?></span>
-						<span class="autor"><?php _e('By') ?> <?php echo get_the_author(); ?></span>
+						<span class="autor"><?php tmblog_posted_by(); ?></span>
 				</div>
 				 <h2 class="entry-title tags-page-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'tmblog' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 				
