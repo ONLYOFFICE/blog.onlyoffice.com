7d6a8211799faa640798af004a9ce280348bfb26
MikhailTyuftyaev <tyuftiaev@mail.ru>
2021-09-10 11:59:26 +0300

    Edit plugin wp-discourse and added translations

--- wp-content/plugins/wp-discourse/lib/discourse-comment-formatter.php
+++ wp-content/plugins/wp-discourse/lib/discourse-comment-formatter.php
@@ -184,6 +184,6 @@ class DiscourseCommentFormatter {
 
 		$link_text = apply_filters( 'wpdc_join_discussion_link_text', $link_text, $comments_count, $post_id );
 
-		return '<div class="wpdc-join-discussion"><a class="wpdc-join-discussion-link" href="' . esc_url_raw( $discourse_permalink ) . '"' . $new_tab . '>' . esc_html_e('Find news, tips and how-tos', 'teamlab-blog-2-0') . '</a></div>';
+		return '<div class="wpdc-join-discussion"><a class="wpdc-join-discussion-link" href="' . esc_url_raw( $discourse_permalink ) . '"' . $new_tab . '>' . __('Join the Discussion', 'teamlab-blog-2-0') . '</a></div>';
 	}
 }
