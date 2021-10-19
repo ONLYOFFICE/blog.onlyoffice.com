76254d486e8b41358116a5144e4bd2fedd215058
MikhailTyuftyaev <mikhail.tuftyaev@onlyoffice.com>
2021-10-19 12:26:32 +0300

    Delete plugin Trinity Audio

--- wp-content/plugins/trinity-audio/js/common.js
+++
@@ -1,4 +0,0 @@
-function trinityShowStatus(id, statusName) {
-  jQuery(`${id} .trinity-status-wrapper .status`).removeClass('show');
-  jQuery(`${id} .trinity-status-wrapper .status.${statusName}`).addClass('show');
-}
--- wp-content/plugins/trinity-audio/migrations/README.md
+++
@@ -1,1 +0,0 @@
-# Plugin Migration scripts
