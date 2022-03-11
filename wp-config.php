700c609dd6fb55f8263cb5b384d18d25ddc77759
Mikhail Tyuftyaev <mikhail.tuftyaev@onlyoffice.com>
2022-03-11 10:58:25 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -34,7 +34,7 @@ define('DB_PASSWORD', 'password-database');
 define('DB_HOST', 'host1');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4_unicode_ci');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
