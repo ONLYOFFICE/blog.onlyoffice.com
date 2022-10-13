64f4c028da20e204f2aac7e557e192c557e3be1d
javlo <javlon@gmail.com>
2022-10-13 12:23:52 +0500

    menu finished with translate footer update 7.2

--- wp-content/themes/teamlab-blog-2-0/footer.php
+++ wp-content/themes/teamlab-blog-2-0/footer.php
@@ -23,62 +23,60 @@ $lang = $sitepress->get_current_language();
             <div class="bottomlines">
                 <ul class="footer_menu <?php echo $lang ?>">
                     <div group-menu>
-                        <li class="footer-border">
-                            <a id="footer_menu_developers" class="footer-button">
-                                <h6><?php _e('Developers', 'teamlab-blog-2-0'); ?></h6>
-                            </a>
-                            <div id="navitem_footer_developers">
-                                <ul>
-                                    <li>
-                                        <p><a href="<?php echo $current_language ?>/developer-edition.aspx"><?php _e('Developer Edition', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a href="<?php echo $current_language ?>/document-builder.aspx"><?php _e('Document Builder', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a href="<?php echo $current_language ?>/groups.aspx"><?php _e('Groups', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a target="_blank" rel="noreferrer noopener" id="navitem_api" href="https://api.onlyoffice.com/"><?php _e('API', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a target="_blank" rel="noreferrer noopener" href="https://github.com/ONLYOFFICE/"><?php _e('Code on GitHub', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li class="footer-border">
-                            <a id="footer_menu_solutions" class="footer-button">
-                                <h6><?php _e('Security', 'teamlab-blog-2-0'); ?></h6>
-                            </a>
-                            <div id="navitem_footer_solutions">
-                                <ul>
-                                    <li>
-                                        <p><a href="<?php echo $current_language ?>/security.aspx#access_control"><?php _e('Authentication and access control', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a href="<?php echo $current_language ?>/security.aspx#data_protection"><?php _e('Data protection', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a href="<?php echo $current_language ?>/security.aspx#data_encryption"><?php _e('Data encryption', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a href="<?php echo $current_language ?>/private-rooms.aspx"><?php _e('Private rooms', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a href="https://www.onlyoffice.com/blog/2018/05/how-onlyoffice-complies-with-gdpr/"><?php _e('GDPR compliance', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                    <li>
-                                        <p><a href="https://www.onlyoffice.com/blog/2020/10/how-onlyoffice-complies-with-hipaa/"><?php _e('HIPAA compliance', 'teamlab-blog-2-0'); ?></a></p>
-                                    </li>
-                                </ul>
-                            </div>
-                        </li>
-                    </div>
-                    <div group-menu>
+<!--                        <li class="footer-border">-->
+<!--                            <a id="footer_menu_developers" class="footer-button">-->
+<!--                                <h6>--><?php //_e('Developers', 'teamlab-blog-2-0'); ?><!--</h6>-->
+<!--                            </a>-->
+<!--                            <div id="navitem_footer_developers">-->
+<!--                                <ul>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="--><?php //echo $current_language ?><!--/developer-edition.aspx">--><?php //_e('Developer Edition', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="--><?php //echo $current_language ?><!--/document-builder.aspx">--><?php //_e('Document Builder', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="--><?php //echo $current_language ?><!--/groups.aspx">--><?php //_e('Groups', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a target="_blank" rel="noreferrer noopener" id="navitem_api" href="https://api.onlyoffice.com/">--><?php //_e('API', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a target="_blank" rel="noreferrer noopener" href="https://github.com/ONLYOFFICE/">--><?php //_e('Code on GitHub', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                </ul>-->
+<!--                            </div>-->
+<!--                        </li>-->
+<!--                        <li class="footer-border">-->
+<!--                            <a id="footer_menu_solutions" class="footer-button">-->
+<!--                                <h6>--><?php //_e('Security', 'teamlab-blog-2-0'); ?><!--</h6>-->
+<!--                            </a>-->
+<!--                            <div id="navitem_footer_solutions">-->
+<!--                                <ul>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="--><?php //echo $current_language ?><!--/security.aspx#access_control">--><?php //_e('Authentication and access control', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="--><?php //echo $current_language ?><!--/security.aspx#data_protection">--><?php //_e('Data protection', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="--><?php //echo $current_language ?><!--/security.aspx#data_encryption">--><?php //_e('Data encryption', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="--><?php //echo $current_language ?><!--/private-rooms.aspx">--><?php //_e('Private rooms', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="https://www.onlyoffice.com/blog/2018/05/how-onlyoffice-complies-with-gdpr/">--><?php //_e('GDPR compliance', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                    <li>-->
+<!--                                        <p><a href="https://www.onlyoffice.com/blog/2020/10/how-onlyoffice-complies-with-hipaa/">--><?php //_e('HIPAA compliance', 'teamlab-blog-2-0'); ?><!--</a></p>-->
+<!--                                    </li>-->
+<!--                                </ul>-->
+<!--                            </div>-->
+<!--                        </li>-->
                         <li class="footer-border">
                             <a id="footer_menu_solutions_bsz" class="footer-button">
-                                <h6><?php _e('By size', 'teamlab-blog-2-0'); ?></h6>
+                                <h6><?php _e('SOLUTIONS BY SIZE', 'teamlab-blog-2-0'); ?></h6>
                             </a>
                             <div id="navitem_footer_by_size">
                                 <ul>
@@ -96,7 +94,7 @@ $lang = $sitepress->get_current_language();
                         </li>
                         <li class="footer-border">
                             <a id="footer_menu_solutions_bid" class="footer-button">
-                                <h6><?php _e('By industry', 'teamlab-blog-2-0'); ?></h6>
+                                <h6><?php _e('SOLUTIONS BY INDUSTRY', 'teamlab-blog-2-0'); ?></h6>
                             </a>
                             <div id="navitem_footer_by_industry">
                                 <ul>
@@ -230,42 +228,49 @@ $lang = $sitepress->get_current_language();
                         </li>
                     </div>
                 </ul>
-                <div class="SocialLinks">
-                    <ul class="ListSocLink">
-                        <li><a class="subscribe-mail" title="Subscribe to our newsletters" id="subscribelink">
-                                <label title="Subscribe to our newsletters" class="social_grey_subscribe"></label>
-                            </a></li>
-                        <li><a target="_blank" href="https://www.facebook.com/pages/OnlyOffice/833032526736775" onmouseup="PageTrack('GoTo_facebook');" rel="noreferrer noopener" title="Follow us on Facebook" class="faceBook">
-                                <label class="social_grey_fb" title="Facebook"></label>
-                            </a></li>
-                        <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_twitter');" href="https://twitter.com/ONLY_OFFICE" title="Follow us on Twitter" class="twitter" rel="noreferrer noopener">
-                                <label class="social_grey_twi" title="Twitter"></label>
-                            </a></li>
-                        <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_linkedin');" href="https://www.linkedin.com/groups/ONLYOFFICE-6726380" title="Follow us on LinkedIn" class="linkedin" rel="noreferrer noopener">
-                                <label class="social_grey_in" title="LinkedIn"></label>
-                            </a></li>
-                        <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_youtube');" href="https://www.youtube.com/user/onlyofficeTV" title="Follow us on YouTube" class="youtube" rel="noreferrer noopener">
-                                <label class="social_grey_tube" title="YouTube"></label>
-                            </a></li>
-                        <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_blog');" href="https://www.onlyoffice.com/blog" title="Read our blog" class="blog" rel="noreferrer noopener">
-                                <label title="Blog" class="social_grey_blog"></label>
-                            </a></li>
-                        <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_medium');" href="https://medium.com/onlyoffice" title="Follow us on Medium" class="medium" rel="noreferrer noopener">
-                                <label title="Medium" class="social_grey_medium"></label>
-                            </a></li>
-                        <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_instagram');" class="medium" title="Follow us on Instagram" href="https://www.instagram.com/the_onlyoffice/" rel="noreferrer noopener">
-                                <label title="Instagram" class="social_grey_instagram"></label>
-                            </a></li>
-                        <li><a class="github" title="Follow us on GitHub" href="https://github.com/ONLYOFFICE/" onmouseup="PageTrack('GoTo_medium');" target="_blank" rel="noreferrer noopener">
-                                <label title="GitHub" class="social_grey_github"></label>
-                            </a></li>
-                        <li><a class="fosstodon" title="Follow us on Fosstodon" href="https://fosstodon.org/@ONLYOFFICE" onmouseup="PageTrack('GoTo_medium');" target="_blank" rel="noreferrer noopener">
-                                <label title="Fosstodon" class="social_grey_fosstodon"></label>
-                            </a></li>
-                    </ul>
-                    <div class="copyReserved">&copy; <?php _e('Ascensio System SIA', 'teamlab-blog-2-0'); ?> <?php echo date("Y"); ?>. <?php _e('All rights reserved', 'teamlab-blog-2-0'); ?></div>
+                <div class="footer_menu_item fmi_social">
+                    <div class="SocialLinks">
+                        <span>Follow us on:</span>
+                        <ul class="ListSocLink">
+                            <li>
+                                <a class="subscribe-mail" title="Subscribe to our newsletters" id="subscribelink">
+                                    <label title="Subscribe to our newsletters" class="social_grey_subscribe"></label>
+                                </a>
+                            </li>
+                            <li><a target="_blank" href="https://www.facebook.com/pages/OnlyOffice/833032526736775" onmouseup="PageTrack('GoTo_facebook');" rel="noreferrer noopener" title="Follow us on Facebook" class="faceBook">
+                                    <label class="social_grey_fb" title="Facebook"></label>
+                                </a></li>
+                            <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_twitter');" href="https://twitter.com/ONLY_OFFICE" title="Follow us on Twitter" class="twitter" rel="noreferrer noopener">
+                                    <label class="social_grey_twi" title="Twitter"></label>
+                                </a></li>
+                            <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_linkedin');" href="https://www.linkedin.com/groups/ONLYOFFICE-6726380" title="Follow us on LinkedIn" class="linkedin" rel="noreferrer noopener">
+                                    <label class="social_grey_in" title="LinkedIn"></label>
+                                </a></li>
+                            <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_youtube');" href="https://www.youtube.com/user/onlyofficeTV" title="Follow us on YouTube" class="youtube" rel="noreferrer noopener">
+                                    <label class="social_grey_tube" title="YouTube"></label>
+                                </a></li>
+                            <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_blog');" href="https://www.onlyoffice.com/blog" title="Read our blog" class="blog" rel="noreferrer noopener">
+                                    <label title="Blog" class="social_grey_blog"></label>
+                                </a></li>
+                            <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_medium');" href="https://medium.com/onlyoffice" title="Follow us on Medium" class="medium" rel="noreferrer noopener">
+                                    <label title="Medium" class="social_grey_medium"></label>
+                                </a></li>
+                            <li><a rel="nofollow" target="_blank" onmouseup="PageTrack('GoTo_instagram');" class="medium" title="Follow us on Instagram" href="https://www.instagram.com/the_onlyoffice/" rel="noreferrer noopener">
+                                    <label title="Instagram" class="social_grey_instagram"></label>
+                                </a></li>
+                            <li><a class="github" title="Follow us on GitHub" href="https://github.com/ONLYOFFICE/" onmouseup="PageTrack('GoTo_medium');" target="_blank" rel="noreferrer noopener">
+                                    <label title="GitHub" class="social_grey_github"></label>
+                                </a></li>
+                            <li><a class="fosstodon" title="Follow us on Fosstodon" href="https://fosstodon.org/@ONLYOFFICE" onmouseup="PageTrack('GoTo_medium');" target="_blank" rel="noreferrer noopener">
+                                    <label title="Fosstodon" class="social_grey_fosstodon"></label>
+                                </a></li>
+                        </ul>
+                    </div>
+                    <div class="copyReserved">
+                        <p>&copy; <?php _e('Ascensio System SIA 1998', 'teamlab-blog-2-0'); ?>-<?php echo date("Y"); ?>.</p>
+                        <p><?php _e('All rights reserved', 'teamlab-blog-2-0'); ?></p>
+                    </div>
                 </div>
-
             </div>
 
         </div>
