edfd01666c266ca90ba8d467f377e12e90f9bfca
Denis <denistyan16@gmail.com>
2023-01-16 23:29:27 +0500

    feat(social-icons): add social items for zh and ja languages

--- wp-content/themes/teamlab-blog-2-0/social-icons.php
+++ wp-content/themes/teamlab-blog-2-0/social-icons.php
@@ -2,6 +2,21 @@
 	<h4 class="widget-title"><?php _e('Follow us', 'teamlab-blog-2-0'); ?></h4>
 	<ul class="social-icons-list">
 		<li class="icons-item"><a id="subscribelink2" class="wdgt-subscribe"></a></li>
+		<?php if ($current_language == WEB_ROOT_URL.'/'.'zh') { ?>
+			<li class="icons-item">
+				<a class="wdgt-wechat" title="WeChat"></a>
+				<div class="popup_qr_code wechat_qr_code">
+						<p>关注我们</p>
+						<p>了解ONLYOFFICE最新信息</p>
+				</div>
+			</li>
+		<?php } ?>
+		<?php if ($current_language == WEB_ROOT_URL.'/'.'ja') { ?>
+			<li class="icons-item">
+				<a class="wdgt-line" title="LINE"></a>
+				<div class="popup_qr_code line_qr_code"></div>
+			</li>
+		<?php } ?>
 		<li class="icons-item"><a class="wdgt-facebook" target="_blank" href="https://www.facebook.com/ONLYOFFICE-833032526736775/"></a></li>
 		<li class="icons-item"><a class="wdgt-twitter" target="_blank" href="https://twitter.com/ONLY_OFFICE"></a></li>
 		<li class="icons-item"><a class="wdgt-linkedin" target="_blank" href="https://www.linkedin.com/groups/ONLYOFFICE-6726380"></a></li>
