672a2c06fd737dc664b0323ed845552020356f4c
Vyacheslav-Belov-OO <63102499+Vyacheslav-Belov-OO@users.noreply.github.com>
2023-02-16 09:46:13 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -97,4 +97,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Инициализирует переменные WordPress и подключает файлы. */
 require_once( ABSPATH . 'wp-settings.php' );
-add_filter('xmlrpc_enabled', '__return_false');
+add_filter('xmlrpc_enabled', '__return_false'); 
