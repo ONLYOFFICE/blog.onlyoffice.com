93c331435d74ce2e7fd79d4dc696d84406f93ea0
Denis <denistyan16@gmail.com>
2023-04-10 12:51:55 +0500

    fix: update WPML Multilingual CMS plugin

--- wp-content/plugins/sitepress-multilingual-cms/res/js/theme-localization.js
+++ wp-content/plugins/sitepress-multilingual-cms/res/js/theme-localization.js
@@ -1,76 +1,60 @@
-addLoadEvent(function(){     
+var WPML_Core = WPML_Core || {};
+WPML_Core.theme_localization = {};
+
+addLoadEvent(function(){
     jQuery('#icl_theme_localization').submit(iclSaveThemeLocalization);
     jQuery('#icl_theme_localization_type').submit(iclSaveThemeLocalizationType);
-    jQuery('#icl_theme_localization_type :radio[name="icl_theme_localization_type"]').change(iclEditThemeLocalizationType);
-    jQuery('#icl_theme_localization_type :checkbox[name="icl_theme_localization_load_td"]').change(iclToggleTextDoomainInput);
-    
-    jQuery(document).delegate('.check-column :checkbox', 'change', iclCheckColumn);
+
+    jQuery(document).on('change', '.check-column-plugin :checkbox', function () {
+        WPML_Core.theme_localization.check_column( 'plugins', jQuery(this).prop('checked') );
+    });
+    jQuery(document).on('change', '.check-column-theme :checkbox', function () {
+        WPML_Core.theme_localization.check_column( 'themes', jQuery(this).prop('checked') );
+    });
 });
 
 function iclSaveThemeLocalization(){
-    var ajx = location.href.replace(/#(.*)$/,'');
-    if(-1 == location.href.indexOf('?')){
-        url_glue='?';
-    }else{
-        url_glue='&';
-    }
-    spl = jQuery(this).serialize().split('&');    
+    var spl = jQuery(this).serialize().split('&');
     var parameters = {};
-    for(var i=0; i< spl.length; i++){        
+    for(var i=0; i< spl.length; i++){
         var par = spl[i].split('=');
-        eval('parameters.' + par[0] + ' = par[1]');
-    }    
-    jQuery('#icl_theme_localization_wrap').load(location.href + ' #icl_theme_localization_subwrap', parameters, function(){
-        fadeInAjxResp('#icl_ajx_response_fn', icl_ajx_saved);                                                 
-    }); 
-    return false;   
+        parameters[par[0]] = par[1];
+    }
+    jQuery('#icl_theme_localization_wrap').load(WPML_core.sanitize(location.href) + ' #icl_theme_localization_subwrap', parameters, function(){
+        fadeInAjxResp('#icl_ajx_response_fn', icl_ajx_saved);
+    });
+    return false;
 }
 
 function iclSaveThemeLocalizationType(){
     jQuery(this).find('.icl_form_errors').fadeOut();
     var val         = jQuery(this).find('[name="icl_theme_localization_type"]:checked').val();
-    var td_on       = jQuery(this).find('[name="icl_theme_localization_load_td"]').attr('checked');
+    var td_on       = jQuery(this).find('[name="icl_theme_localization_load_td"]').prop('checked');
     var td_value    = jQuery(this).find('[name="textdomain_value"]').val();
 
-    if(val == 2 && td_on && !jQuery.trim(td_value)){
+    if(val == 2 && td_on && !td_value.trim()){
         jQuery(this).find('.icl_form_errors_1').fadeIn();
         return false;
     }
-    
+
+    var data = jQuery(this).serializeArray();
+    data.push({
+        'name': 'action',
+        'value' : 'WPML_Theme_Localization_Type'
+    });
+
     jQuery.ajax({
         type: "POST",
-        url: icl_ajx_url,
-        data: "icl_ajx_action="+jQuery(this).attr('name')+"&"+jQuery(this).serialize(),
-        success: function(msg){
-            spl = msg.split('|');
-            location.href=location.href.replace(/#(.*)$/,'');
+        url: ajaxurl,
+        data: data,
+        success: function(){
+            location.href = WPML_core.sanitize(location.href).replace(/#(.*)$/,'');
         }
     });
-    return false;         
+    return false;
 }
 
-function iclCheckColumn(){
-    if(jQuery(this).attr('checked')){
-        jQuery('#icl_strings_in_plugins :checkbox').attr('checked','checked');
-    }else{
-        jQuery('#icl_strings_in_plugins :checkbox').removeAttr('checked');
-    }    
-}
-
-function iclEditThemeLocalizationType(){
-    var val = jQuery(this).val();
-    if(val == 2){
-        jQuery('#icl_tt_type_extra').fadeIn();        
-    }else{
-        jQuery('#icl_tt_type_extra').fadeOut();
-    }
-}
-
-function iclToggleTextDoomainInput(){
-    var checked = jQuery(this).attr('checked');
-    if(checked){
-        jQuery('#icl_tt_type_extra_td').fadeIn();    
-    }else{
-        jQuery('#icl_tt_type_extra_td').fadeOut();
-    }
-}
+WPML_Core.theme_localization.check_column = function(type, checked){
+    var visible_rows = jQuery('#wpml_strings_in_' + type).find(':checkbox:visible');
+    visible_rows.prop('checked', checked);
+};
